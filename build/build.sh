b59fc1c32b759cb7334eddd4858061add9af0469
cncsmonster <cncsmonster@163.com>
2023-08-30 09:21:58 +0000

    mkdir build,write build shell

---
+++ build/build.sh
@@ -0,0 +1,5 @@
+# 编译产生各个可执行模块的可执行文件
+# 产生cli
+go build ../cli/nd-cli
+go build ../cli/pc-cli
+
---
+++ cli/nd-cli/pc-checker-cli.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+// 检查pc推理机器
+func pc_checker_cli() {
+	app := &cli.App{
+		Name:   "pc checker",
+		Usage:  "make an pc proof checker",
+		Action: PcCheckAction,
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func PcCheckAction(ctx *cli.Context) error {
+	fmt.Println("gg")
+	return nil
+}
